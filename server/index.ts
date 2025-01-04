8a68c6e43b51e7d231383ed583833e99691dcc00
ganeshPund7787 <ganupund7787@gmail.com>
2025-01-04 10:26:54 +0530

    change origin

--- server/index.ts
+++ server/index.ts
@@ -18,13 +18,13 @@ const PORT = process.env.PORT || 3000;
 
 const DIRNAME = path.resolve();
 
-
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin: "https://cook4you.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
